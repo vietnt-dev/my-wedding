4bc86abee0c6fd5eff5753d263c9313ac86a5c37
vietnt <viet.nguyen@sotatek.com>
2024-10-06 20:45:45 +0700

    first commit

--- js/custom.js
+++ js/custom.js
@@ -238,38 +238,36 @@
 	
 	// CountDown Js
 	var deadline = 'April 28 2024 GMT+7';
-		function time_remaining(endtime){
-			var t = Date.parse(endtime) - Date.parse(new Date());
-			var seconds = Math.floor( (t/1000) % 60 );
-			var minutes = Math.floor( (t/1000/60) % 60 );
-			var hours = Math.floor( (t/(1000*60*60)) % 24 );
-			var days = Math.floor( t/(1000*60*60*24) );
-			return {'total':t, 'days':days, 'hours':hours, 'minutes':minutes, 'seconds':seconds};
-		}
-		function run_clock(id,endtime){
-			var clock = document.getElementById(id);
-			
-			// get spans where our clock numbers are held
-			var days_span = clock.querySelector('.days');
-			var hours_span = clock.querySelector('.hours');
-			var minutes_span = clock.querySelector('.minutes');
-			var seconds_span = clock.querySelector('.seconds');
+	function time_elapsed(starttime){
+		var t = Date.parse(new Date()) - Date.parse(starttime);
+		var seconds = Math.floor( (t/1000) % 60 );
+		var minutes = Math.floor( (t/1000/60) % 60 );
+		var hours = Math.floor( (t/(1000*60*60)) % 24 );
+		var days = Math.floor( t/(1000*60*60*24) );
+		return {'total':t, 'days':days, 'hours':hours, 'minutes':minutes, 'seconds':seconds};
+	}
+	function run_clock(id,starttime){
+		var clock = document.getElementById(id);
+		
+		// get spans where our clock numbers are held
+		var days_span = clock.querySelector('.days');
+		var hours_span = clock.querySelector('.hours');
+		var minutes_span = clock.querySelector('.minutes');
+		var seconds_span = clock.querySelector('.seconds');
 
-			function update_clock(){
-				var t = time_remaining(endtime);
-				
-				// update the numbers in each part of the clock
-				days_span.innerHTML = t.days;
-				hours_span.innerHTML = ('0' + t.hours).slice(-2);
-				minutes_span.innerHTML = ('0' + t.minutes).slice(-2);
-				seconds_span.innerHTML = ('0' + t.seconds).slice(-2);
-				
-				if(t.total<=0){ clearInterval(timeinterval); }
-			}
-			update_clock();
-			var timeinterval = setInterval(update_clock,1000);
+		function update_clock(){
+			var t = time_elapsed(starttime);
+			
+			// update the numbers in each part of the clock
+			days_span.innerHTML = t.days;
+			hours_span.innerHTML = ('0' + t.hours).slice(-2);
+			minutes_span.innerHTML = ('0' + t.minutes).slice(-2);
+			seconds_span.innerHTML = ('0' + t.seconds).slice(-2);
 		}
-		run_clock('clockdiv',deadline);	
+		update_clock();
+		var timeinterval = setInterval(update_clock,1000);
+	}
+	run_clock('clockdiv',deadline);	
 	
 	
 	// Contact Form Submition
@@ -342,4 +340,4 @@
 	});
 	
 	});
-})(); 
+})();
